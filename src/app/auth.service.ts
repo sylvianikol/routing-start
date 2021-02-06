8cdc91684ecc30d2ae1d8b00208512fd2455b8b3
sylvianikol <silviaadvert@gmail.com>
2021-02-06 11:11:37 +0200

    added auth guard

---
+++ src/app/auth.service.ts
@@ -0,0 +1,25 @@
+import { fakeAsync } from "@angular/core/testing";
+
+export class AuthService {
+    loggedIn = false;
+
+    isAuthenticated() {
+        const promise = new Promise(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn);
+                }, 800)
+            }
+        );
+
+        return promise;
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
